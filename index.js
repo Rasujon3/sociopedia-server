c8626618859bd15ed426ff938b2a99ec9635595b
Rasujon3 <rasujon3@gmail.com>
2023-01-07 02:40:14 +0600

    authentication & authorization & user route setup done

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,13 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// Routes with files
+app.post("/auth/register", upload.single("picture"), register);
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // Mongoose Setup
 const PORT = process.env.PORT || 5001;
